3a5b8ee86167018295c7afb859c758b0b3e9d149
Zabihullah Azadzoi <zabihullahazadzoi848249@gmail.com>
2023-01-06 21:55:43 +0530

    final commit

--- server.js
+++ server.js
@@ -55,7 +55,6 @@ io.on("connection", (socket) => {
   });
 
   socket.on("processing", ({ roomId, isProcessing }) => {
-    console.log(isProcessing);
     socket.in(roomId).emit("processing", isProcessing);
   });
 
